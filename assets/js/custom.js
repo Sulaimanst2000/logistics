6459a410967f3bc7bba139ec2fbaad4908d8c905
sulaimanst2000 <sulaimantasneemst@gmail.com>
2023-08-30 04:14:58 +0500

    final-upload

---
+++ assets/js/custom.js
@@ -0,0 +1,77 @@
+$('.logo-slide').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 5,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 300,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                dots: false,
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+$('.test').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    autoplay: true,
+    // arrows: false,
+    prevArrow:'.prev',
+    nextArrow:'.next',
+    autoplaySpeed: 1500,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                dots: false,
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+
+
